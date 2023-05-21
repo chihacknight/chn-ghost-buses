8ec26faaee70bcceb5cf6df04dbef686aaca4561
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2023-05-21 10:09:49 -0500

    Turn off cronjob

--- test_script.py
+++ test_script.py
@@ -10,7 +10,7 @@ client = boto3.client(
     aws_secret_access_key=SECRET_KEY
 )
 
-client.upload_file("test_file.txt", 'chn-ghost-buses-public', "est_file.txt")
+client.upload_file("test_file.txt", 'chn-ghost-buses-public', "test_file.txt")
 
 print(client.list_objects_v2(
     Bucket='chn-ghost-buses-public'

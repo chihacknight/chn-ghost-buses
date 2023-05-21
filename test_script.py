7edd22fc048697cf0ca7de93769269208b974e91
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2023-05-21 09:11:48 -0500

    Test creation of boto s3 client

--- test_script.py
+++ test_script.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import boto3
+import sys
+
+ACCESS_KEY = sys.argv[1]
+SECRET_KEY = sys.argv[2]
+
+client = boto3.client(
+    's3',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
+)
+
+print(client.list_objects_v2(
+    Bucket='chn-ghost-buses-public'
+))

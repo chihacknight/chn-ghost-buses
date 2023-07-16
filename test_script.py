05182933604bca3b316a7f08b84c723d9913a87b
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2023-07-16 13:45:57 -0500

    List files before uploading file to s3

--- test_script.py
+++ test_script.py
@@ -10,8 +10,8 @@ client = boto3.client(
     aws_secret_access_key=SECRET_KEY
 )
 
-client.upload_file("test_file.txt", 'chn-ghost-buses-public', "test_file.txt")
-
 print(client.list_objects_v2(
     Bucket='chn-ghost-buses-public'
 ))
+
+client.upload_file("test_file.txt", 'chn-ghost-buses-public', "test_file.txt")

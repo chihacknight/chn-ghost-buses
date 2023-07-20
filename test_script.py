a50c053f814a738390a63d7bc05113b8d371c0a1
laurie merrell <lauriemerrell@gmail.com>
2023-07-19 20:24:17 -0500

    try write again with new perms added in aws console

--- test_script.py
+++ test_script.py
@@ -10,17 +10,17 @@ client = boto3.client(
     aws_secret_access_key=SECRET_KEY
 )
 
-print("Printing bucket objects:")
-print(client.list_objects_v2(
-    Bucket='chn-ghost-buses-private',
-    Prefix='bus_full_day_data_v2/'
-))
+# print("Printing bucket objects:")
+# print(client.list_objects_v2(
+#     Bucket='chn-ghost-buses-public',
+#     Prefix='bus_full_day_data_v2/'
+# ))
 
-# s3 = boto3.resource(
-#     's3',
-#     region_name='us-east-1',
-#     aws_access_key_id=ACCESS_KEY,
-#     aws_secret_access_key=SECRET_KEY
-# )
-# content="String content to write to a new S3 file"
-# s3.Object('chn-ghost-buses-public', 'newfile.txt').put(Body=content)
+s3 = boto3.resource(
+    's3',
+    region_name='us-east-1',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
+)
+content="String content to write to a new S3 file"
+s3.Object('chn-ghost-buses-public', 'newfile.txt').put(Body=content)

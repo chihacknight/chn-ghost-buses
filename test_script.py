8b8605aadd5c6fbe2e1add4056fe75c4e2a6e6e4
laurie merrell <lauriemerrell@gmail.com>
2023-07-19 19:58:51 -0500

    test iam client

--- test_script.py
+++ test_script.py
@@ -4,27 +4,25 @@ import sys
 ACCESS_KEY = sys.argv[1]
 SECRET_KEY = sys.argv[2]
 
-client = boto3.client(
-    's3',
-    # aws_access_key_id=ACCESS_KEY,
-    # aws_secret_access_key=SECRET_KEY
-    aws_access_key_id = "asdfdafsafds",
-    aws_secret_access_key = "asfdafds"
-)
-
-print("Printing bucket objects:")
-print(client.list_objects_v2(
-    Bucket='chn-ghost-buses-public'
-))
-
-print("The user is:")
-print(client.get_caller_identity().get('Account'))
-
-s3 = boto3.resource(
-    's3',
-    region_name='us-east-1',
+iam_client = boto3.client(
+    'IAM',
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_KEY
 )
-content="String content to write to a new S3 file"
-s3.Object('chn-ghost-buses-public', 'newfile.txt').put(Body=content)
+
+# print("Printing bucket objects:")
+# print(client.list_objects_v2(
+#   Bucket='chn-ghost-buses-public'
+# ))
+
+print("The user is:")
+print(iam_client.get_user())
+
+# s3 = boto3.resource(
+#     's3',
+#     region_name='us-east-1',
+#     aws_access_key_id=ACCESS_KEY,
+#     aws_secret_access_key=SECRET_KEY
+# )
+# content="String content to write to a new S3 file"
+# s3.Object('chn-ghost-buses-public', 'newfile.txt').put(Body=content)

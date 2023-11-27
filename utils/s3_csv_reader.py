3305dd8990bdc4fe4a685651dcb6dfe2b04bc88e
dcjohnson24 <dcjohnson24@gmail.com>
2023-11-26 18:24:03 -0600

    Remove try-except block

--- utils/s3_csv_reader.py
+++ utils/s3_csv_reader.py
@@ -1,10 +1,9 @@
 import pandas as pd
 from pathlib import Path
-import botocore
 import data_analysis.compare_scheduled_and_rt as csrt
 
 def read_csv(filename: str | Path) -> pd.DataFrame:    
-    """Read pandas csv from S3 using multiple methods
+    """Read pandas csv from S3
 
     Args:
         filename (str | Path): file to download from S3.
@@ -14,13 +13,10 @@ def read_csv(filename: str | Path) -> pd.DataFrame:
     """
     if isinstance(filename, str):
         filename = Path(filename)
-    # Use low_memory option to avoid warning about columns with mixed dtypes.
-    try:
-        df = pd.read_csv(filename.as_uri(), low_memory=False)
-    except (botocore.exceptions.ConnectTimeoutError, botocore.exceptions.EndpointConnectionError):
-        s3_filename = '/'.join(filename.parts[-2:])
-        df = pd.read_csv(
+    s3_filename = '/'.join(filename.parts[-2:])
+    df = pd.read_csv(
             f'https://{csrt.BUCKET_PUBLIC}.s3.us-east-2.amazonaws.com/{s3_filename}',
             low_memory=False
         )
     return df
+    

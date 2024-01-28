32727ef5b9ba5ea35bce760d623d034700848b9b
dcjohnson24 <dcjohnson24@gmail.com>
2024-01-28 15:43:45 -0600

    Improved holiday handling. Typer. Add function to check date of existing ridership data

--- data_analysis/ridership_to_json.py
+++ data_analysis/ridership_to_json.py
@@ -2,8 +2,11 @@ import os
 from pathlib import Path
 import pandas as pd
 import calendar
-# Typer doesn't support pandas DataFrames
-import argparse
+import typing
+import typer
+import json
+import requests
+import sys
 
 project_name = os.getenv('PROJECT_NAME', 'chn-ghost-buses')
 current_dir = Path(__file__)
@@ -14,10 +17,30 @@ project_dir = next(
 
 DATA_PATH = project_dir / 'data_output' / 'scratch'
 
-ridership = pd.read_csv('https://data.cityofchicago.org/api/views/jyb9-n7fm/rows.csv?accessType=DOWNLOAD')
+def check_existing_date() -> typing.Optional[typing.Tuple[int]]:
+    """Fetch the date of the ridership data currently in main branch of 
+    frontend repo.
 
+    Returns:
+        typing.Optional[typing.Tuple[int]]: A tuple of month and year of the date 
+            of the data in main branch. Returns None if no data is found.
+    """
+    # Grab the latest date from GitHub. URL doesn't work until frontend 
+    # PR #129 merged into main.
+    url = 'https://github.com/chihacknight/ghost-buses-frontend/main/src/Routes/cta_ridership_data_day_type_summary.json'
+    json_data = requests.get(url).json()
+    if 'error' in json_data.keys():
+        print(f"Data not found at {url}")
+        return
+    json_data_gh = json_data['payload']['blob']['rawLines'][0]
+    website_date = json.loads(json_data_gh)['date']
+    month, year = website_date.split(' ')
+    month_int = list(calendar.month_name).index(month)
+    year_int = int(year)
+    return month_int, year_int
+    
 
-def get_latest_month_and_year(ridership: pd.DataFrame) -> tuple:
+def get_latest_month_and_year(ridership_df: pd.DataFrame) -> tuple:
     """Return the most recent month and year of the ridership data
 
     Args:
@@ -35,12 +58,13 @@ def get_latest_month_and_year(ridership: pd.DataFrame) -> tuple:
     Returns:
         tuple: A month, year tuple
     """
-    ridership.loc[:, 'date'] = pd.to_datetime(ridership.loc[:, 'date'], format="%m/%d/%Y")
+    ridership = ridership_df.copy()
+    ridership.loc[:, 'date'] = pd.to_datetime(ridership.loc[:, 'date'], infer_datetime_format=True)
     latest_date = ridership['date'].max()
     return latest_date.month, latest_date.year
 
 
-def ridership_to_json(ridership: pd.DataFrame, month: int = None, year: int = None) -> None:
+def ridership_to_json(ridership_df: pd.DataFrame, month: int = None, year: int = None) -> None:
     """
     Save ridership data to JSON for given month and year.
     Note that the data is typically a few months 
@@ -60,26 +84,58 @@ def ridership_to_json(ridership: pd.DataFrame, month: int = None, year: int = No
         month (int): Month of interest. Defaults to None
         year (int): Year of interest. Defaults to None
     """
+    ridership = ridership_df.copy()
     latest_month, latest_year = get_latest_month_and_year(ridership)
     if month is None:
         month = latest_month
     if year is None:
         year = latest_year
     month_name = calendar.month_name[month]
+    
+    existing_date = check_existing_date()
+    if isinstance(existing_date, tuple):
+        existing_month, existing_year = existing_date
+        if month == existing_month and year == existing_year:
+            print("No new ridership data. Exiting now.")
+            sys.exit(0)
+        else:
+            print("New ridership data available")
+    else:
+        print(f"Using ridership data for {month} {year}")
+
+    # Holidays that are the same day every year
+    hols = ['12/25', '07/04', '01/01']
+    ridership.loc[ridership.date.str.contains('|'.join(hols)), 'day_type'] = 'hol'
+
     ridership['date'] = pd.to_datetime(ridership.date, format="%m/%d/%Y")
     ridership.rename({'route': 'route_id'}, axis=1, inplace=True)
+    ridership['day_type'] = ridership.daytype.map({'W': 'weekday', 'A': 'sat', 'U': 'sun'})
+    
     df = ridership[(ridership['date'].dt.month == month) & (ridership['date'].dt.year == year)].copy()
-    df['day_type'] = df.daytype.map({'W': 'weekday', 'A': 'sat', 'U': 'sun'})
-    df.loc[df.date == '2022-07-04', 'day_type'] = 'hol'
+    
     df[['route_id', 'date', 'day_type', 'rides']].to_json(DATA_PATH / f'daily_{month_name}_{year}_cta_ridership_data.json', orient = 'records')
     df_daytype_summary = df.groupby(by = ['route_id', 'day_type']).agg({'rides': ['mean', 'sum']}).reset_index()
     df_daytype_summary.columns = ['route_id', 'day_type', 'avg_riders', 'total_riders']
-    df_daytype_summary.to_json(DATA_PATH / f'{month_name}_{year}_cta_ridership_data_day_type_summary.json', orient = 'records')
+    df_daytype_summary_json = df_daytype_summary.to_json(orient='records')
+    full_json = {'date': f'{month_name} {year}'}
+    full_json['data'] = json.loads(df_daytype_summary_json)
+    with open(DATA_PATH / f'{month_name}_{year}_cta_ridership_data_day_type_summary.json', 'w') as outfile:
+        json.dump(full_json, outfile)
+
+app = typer.Typer()
+
+@app.command()
+def main(month: int = None, year: int = None) -> None:
+    
+    print("Loading data from data.cityofchicago.org")
+    ridership_df = pd.read_csv(
+        'https://data.cityofchicago.org/api/views/'
+        'jyb9-n7fm/rows.csv?accessType=DOWNLOAD'
+    )
+    print("Done!")
+    ridership_to_json(ridership_df=ridership_df, month=month, year=year)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--month", type=int, help="Month of ridership data")
-    parser.add_argument("--year", type=int, help="Year of ridership data")
-    args = parser.parse_args()
-    ridership_to_json(ridership=ridership, month=args.month, year=args.year)
+    app()
+    

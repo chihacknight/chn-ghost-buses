6da0158ed243f4a668e090c1292538182dfd151f
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2024-05-19 19:30:46 -0500

    Add test for lineplot

--- data_analysis/tests/test_plots.py
+++ data_analysis/tests/test_plots.py
@@ -1,6 +1,29 @@
+import plotly.graph_objects
 from data_analysis.plots import create_save_path
+from data_analysis.plots import lineplot
 import pandas as pd
 
+class MockFig:
+    def add_trace(_, __):
+        pass
+    def write_html(_, __):
+        pass
+    def write_image(_, __):
+        pass
+    
 def test_create_save_path():
     result = create_save_path(save_name="save_name")
     assert "save_name" in result
+
+def test_lineplot(mocker):
+    mocked_plot = mocker.patch('chart_studio.plotly.plot')
+
+    lineplot(
+        x="x",
+        y="y",
+        data=pd.DataFrame(
+            {'x': [1, 2], 'y': [3, 4]}
+        )
+    )
+    
+    assert mocked_plot.called
